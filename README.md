d5e90e724195c1fdd89377cbddfdb158f45a576b
jahan-addison <jahan.addison@jacata.me>
2017-12-12 14:30:11 -0500

    Rewritten with new, cleaner API and interface

--- README.md
+++ README.md
@@ -1,67 +1,39 @@
-# Matzen
-> Simple, functional, pattern matcher in TypeScript.
-
+# Minta
+> Simple, effective, pattern matcher.
 
 ## What is pattern matching?
-
 
 > Pattern matching is the act of checking a given sequence of tokens for the presence of the constituents of some pattern. In contrast to pattern recognition, the match usually has to be **exact**. The patterns have the form of either sequences or tree structures. Uses of pattern matching include outputting the locations (if any) of a pattern within a token sequence, to output some component of the matched pattern, and to substitute the matching pattern with some other token sequence (i.e., search and replace).
 
 ## Details
 
-Matzen was born from recent graphical development. It was inspired by the pattern matching systems in Swift and Haskell.
+Minta was born from recent graphical development. It was inspired by the pattern matching systems in Swift and Haskell.
 
-You may use this project without typescript, and is installable via `npm install matzen`
-
-To build the project, run `npm run build`.  
+To build the project, run `npm run build`.
 To run the test suite, run `npm test`.
 
 
-## API
-
-The package exports an easy-to-use `match` wrapper function which takes a primitive value or finite list (tuple). It allows you to individually match values, tuples, or conditions — and deal with their cases, while optionally 'passing-through' a newly parsed value to the next case.
-
-Similar to Swift, each case chain **must** end with a `default` or (alias) `_()` call. By default, `_()` calls itself with the `identity` function, returning the last matched case value (or fall-through value). `default` may also be called with a final match case callback.
-
-### Basic Examples
-
-```javascript
-const amount = 8;
-const a = match(amount)
-  .case(amount < 5, x => "You don't have enough to pay for that!")
-  .case(5, x => "This is the right amount!")
-  ._(x => "Too much money!");
-
-console.log(a); // "Too much money!"
-```
-
-```javascript
-const a = match([1,2,3])
-  .case([1,2,3], x => [3,2,1], true) // fall-through as new value
-  .case([3,2,1], x => [...x, true, false], true) // fall-through as new value
-  ._();
-
-console.log(a); // [3, 2, 1, true, false]
-```
 
 ### Real world examples
 
 ```javascript
 // clamp
-const a = match(value)
-  .case(value < min, _ => min)
-  .case(value < max, _ => value)
-  .default(_ => max)
+const a = match(value) (
+  value < min, _ => min,
+  value < max, _ => value,
+  _ => max
+);
 ```
 
 ```javascript
 // fib
 function fib(n) {
-  return match(n)
-    .case(0, x => 1)
-    .case(1, x => 1)
-    .case(n >= 2, x => fib(x-1) + fib(x-2))
-    ._();
+  return match(n) (
+    0, x => 1,
+    1, x => 1,
+    n >= 2, x => fib(x-1) + fib(x-2),
+    _ => n
+  );
 }
 ```
 
@@ -72,11 +44,12 @@ class Example {
   }
 }
 const example = new Example();
-const action = match(example)
-  .case(RegExp,  () => 'a regex')
-  .case(String,  () => 'a string')
-  .case(Example, (e) => e.do())
-  ._();
+const action = match(example) (
+  RegExp,  () => 'a regex',
+  String,  () => 'a string',
+  Example, (e) => e.do(),
+  () => false
+);
 
 console.log(action) // 'thing'
 ```
--- test.ts
+++ test.ts
@@ -1,108 +1,91 @@
 import test from 'ava';
-import {Value, Match, match, value} from './matzen';
+import {match, Test, Types} from './minta';
 
 test.beforeEach(t => {
   t.context.data = {
-    0: <value>[1,2,3],  1: 1,  2: new Value([1,2,3]),
-    3: new Match([1,2,3]), 4: new Match([3,2,1], true),
-    5: new Match([new String('test')])
+    0: [1,2,3],
+    1: 1,
+    2: String,
+    3: /tes?ting/,
+    4: 5 === 5,
+    5: "hello world"
   };
 });
+let called = 0;
+function dumb(x: any, reset?: number) {
+  called++;
+  return 'yes';
+}
 
-test('Value::toValue should return turn values into <value> tuples', t => {
-  t.deepEqual(Value.toValue(t.context.data[0]), new Value([1,2,3]));
-  t.deepEqual(Value.toValue(t.context.data[1]), new Value([1]));
-});
-
-test('Value#eq should give equality of Value\'s', t => {
-	t.true(t.context.data[2].eq(new Value([1,2,3])));
-	t.false(t.context.data[2].eq(new Value([3,2,1])));
-});
-
-test('Value#map should map function: callback to Value<value>', t => {
-  t.deepEqual(t.context.data[2].map(x => ++x), new Value([2,3,4]))
-});
-
-test('Value#apply should apply function: callback to Value<value>', t => {
-  t.deepEqual(t.context.data[2].apply(_ => 5), new Value([5]))
-});
-
-test('Value#read should read <value> data from Value<value', t => {
-  t.deepEqual(t.context.data[2].read(), <value>[1,2,3]);
-});
-
-test('Match#case should apply function: callback on boolean pattern', t => {
-  t.context.data[3].case(true, _ => [3,2,1]);
-  t.deepEqual(t.context.data[3].value, new Value([3,2,1]));
-});
-
-test('Match#case should apply function: callback on boolean and not fall through by default', t => {
-  t.context.data[3].case(true, _ => [3,2,1]);
-  t.context.data[3].case(true, _ => [4,5,6]);
-  t.deepEqual(t.context.data[3].value, new Value([3,2,1]));
-});
-
-test('Match#case should apply function: callback on boolean and can fall through', t => {
-  t.context.data[3].case(true, _ => [3,2,1], true);
-  t.context.data[3].case(true, _ => [4,5,6], true);
-  t.deepEqual(t.context.data[3].value, new Value([4,5,6]));
-});
-
-test('Match#case should apply function on instance of match', t => {
-  t.is(t.context.data[5].case(String, () => true)._(), true);
+test('#isTuple should return true on arrays', t => {
+  t.true(Test.isTuple(t.context.data[0]));
+  t.false(Test.isTuple(5));
 })
 
-test('Match#case should apply function: callback on equality of <value> tuple', t => {
-  t.context.data[3].case(<value>[1,2,3], _ => [3,2,1]);
-  t.deepEqual(t.context.data[3].value, new Value([3,2,1]));
-});
-
-test('Match#case should apply function: callback on equality of <value> tuple and not fall through by default', t => {
-  t.context.data[3].case([1,2,3], _ => [3,2,1]);
-  t.context.data[3].case([3,2,1], _ => [4,5,6]);
-  t.deepEqual(t.context.data[3].value, new Value([3,2,1]));
-});
-
-test('Match#case should apply function: callback on equality of <value> tuple and can fall through', t => {
-  t.context.data[3].case([1,2,3], _ => [3,2,1], true);
-  t.context.data[3].case([3,2,1], _ => [true, false], true);
-  t.deepEqual(t.context.data[3].value, new Value([true, false]));
-});
-
-test('Match#case should apply function: callback on 1st value of tuple on 1-tuple', t => {
-  t.deepEqual(new Value([t.context.data[1]]).apply(_ => _ + 1), new Value([2]));
-});
-
-test('Match#case should apply function: callback and pass whole tuple when n > 1', t => {
-  t.deepEqual(new Value(t.context.data[3]).apply(_ => [3,2,1]), new Value([3,2,1]));
+test('#isTuple should return true on array-like objects', t => {
+  t.true(Test.isTuple(t.context.data[5]))
+  t.false(Test.isTuple(t.context.data));
 });
 
 
-test('Match#_ should apply identity by default and return 1st value on 1-tuple', t => {
-  t.is(new Match([1])._(), 1);
+test('#is should return the integer type', t => {
+  const stubs = t.context.data;
+  t.is(Test.is(stubs[4]), Types.Boolean);
+  t.is(Test.is(stubs[3]), Types.RegExp);
+  t.is(Test.is(stubs[2]), Types.Constructor);
+  t.is(Test.is(stubs[0]), Types.Tuple);
+  t.is(Test.is(stubs[5]), Types.Tuple);
+  t.is(Test.is(stubs[1]), Types.Primitive);
+})
+
+test('#eq should return true shallow equality', t => {
+  t.true(Test.eq(t.context.data[0], [1,2,3]));
+  t.false(Test.eq(t.context.data[0], [1,2,4]));
+  t.false(Test.eq(t.context.data[0], [1,2,3,4,5]));
 });
 
-test('Match#_ should apply function: callback and return 1st value of 1-tuple', t => {
-  t.is(new Match([1])._(_ => 5), 5);
+test('#apply should apply function based on type and equality', t => {
+  const apply = Test.apply;
+  const stubs = t.context.data;
+  called = 0;
+  t.is(apply(stubs[0], [1,2,3], dumb), 'yes');
+  t.is(apply(stubs[1], 1, dumb), 'yes');
+  t.is(apply(stubs[2], String, dumb), 'yes');
+  t.is(apply("testing", stubs[3], dumb), 'yes');
+  t.is(apply(stubs[4], true, dumb), 'yes');
+  t.is(apply(stubs[5], "hello world", dumb), 'yes');
+  t.is(apply(stubs[0], [3,2,1], dumb), false);
+  t.is(called, 6);
 });
 
-test('Match#_ should apply identity by default and can will tuple', t => {
-  t.deepEqual(t.context.data[3]._(), [1,2,3]);
+test('#match should error with no default case', t => {
+  t.throws(() => {
+    return match('test') (
+    'test', () => 5,
+  )}, SyntaxError);
 });
 
-test('Match#isFallable should return whether matching instance falls through', t => {
-  t.true(t.context.data[4].isFallable());
-  // the case when a match has been met.
-  t.true(t.context.data[3].isFallable());
-  t.context.data[3].case([1,2,3], _ => [3,2,1]);
-  t.false(t.context.data[3].isFallable());
-  // a match was met, but fallthrough is overwritten
-  t.true(t.context.data[3].isFallable(true));
+test('#match should only apply the first matched case', t => {
+  t.is(match('test') (
+    'test', () => 'yes',
+    Number, () => 'no',
+    _ => 'default'
+  ), 'yes');
 });
 
-// integration
+test('#match should apply the default if there were no matches', t => {
+  t.is(match('test') (
+    'abc', () => 'yes',
+    'def', () => 'no',
+    _ => 'default'
+  ), 'default');
+});
 
-test('match wrapper function', t => {
-  t.deepEqual(match(5), new Match([5], false));
-  t.deepEqual(match([1,2,3], true), new Match([1,2,3], true));
+test('#match should apply all matched cases on passthrough', t => {
+  t.is(match('test', true) (
+    'test',  () => 'yes',
+    'yes',   () => 'super',
+    'silly', () => 'dumb',
+    _ => 'default'
+  ), 'super');
 });
