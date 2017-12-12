00263b3ffb38d32fe9f283e20fee91f807e7acd8
jahan-addison <jahan.addison@jacata.me>
2017-12-12 14:42:02 -0500

    Rewritten with new, cleaner API and interface

--- dist/minta.js
+++ dist/minta.js
@@ -1,4 +1,5 @@
 "use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 var Types;
 (function (Types) {
@@ -13,21 +14,17 @@ function isTuple(x) {
     return x.hasOwnProperty('length');
 }
 function is(x) {
-    if (typeof x === 'boolean')
-        return Types.Boolean;
-    if (x instanceof RegExp)
-        return Types.RegExp;
-    if (typeof x === 'function' && x.constructor.name)
-        return Types.Constructor;
-    if (isTuple(x))
-        return Types.Tuple;
+    if (typeof x === 'boolean') return Types.Boolean;
+    if (x instanceof RegExp) return Types.RegExp;
+    if (typeof x === 'function' && x.constructor.name) return Types.Constructor;
+    if (isTuple(x)) return Types.Tuple;
     return Types.Primitive;
 }
 function eq(x, y) {
     if (x.length !== y.length) {
         return false;
     }
-    return x.reduce((is, item, index) => {
+    return x.reduce(function (is, item, index) {
         return is && item === y[index];
     }, true);
 }
@@ -41,37 +38,39 @@ function apply(initial, test, application) {
             console.log(test.name, initial.constructor.name);
             return test.name === (initial.name || initial.constructor.name) ? application(initial) : false;
         case Types.Tuple:
-            const t1 = Array.prototype.concat.apply([], [initial]);
-            const t2 = Array.prototype.concat.apply([], [test]);
+            var t1 = Array.prototype.concat.apply([], [initial]);
+            var t2 = Array.prototype.concat.apply([], [test]);
             return eq(t1, t2) ? application(initial) : false;
         default:
             return initial === test ? application(initial) : false;
     }
 }
-function match(pattern, passthrough = false) {
-    return (...cases) => {
-        let ret;
-        if (!(cases.length % 2)) {
+function match(pattern) {
+    var passthrough = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+    return function () {
+        var ret = void 0;
+        if (!(arguments.length % 2)) {
             throw new SyntaxError('length of patterns and cases must be odd');
         }
-        for (let i = 0; i < cases.length - 1; i += 2) {
+        for (var i = 0; i < arguments.length - 1; i += 2) {
             if (passthrough) {
-                let value = apply(i > 0 ? ret : pattern, cases[i + 0], cases[i + 1]);
+                var value = apply(i > 0 ? ret : pattern, arguments.length <= i + 0 ? undefined : arguments[i + 0], arguments.length <= i + 1 ? undefined : arguments[i + 1]);
                 if (value) {
                     ret = value;
                 }
+            } else {
+                ret = apply(pattern, arguments.length <= i + 0 ? undefined : arguments[i + 0], arguments.length <= i + 1 ? undefined : arguments[i + 1]);
             }
-            else {
-                ret = apply(pattern, cases[i + 0], cases[i + 1]);
-            }
-            if (ret !== false && passthrough === false)
-                break;
+            if (ret !== false && passthrough === false) break;
         }
         if (!ret) {
-            if (typeof cases[cases.length - 1] !== 'function') {
+            var _ref, _ref2;
+
+            if (typeof (_ref = arguments.length - 1, arguments.length <= _ref ? undefined : arguments[_ref]) !== 'function') {
                 throw new SyntaxError('no default case callback was provided');
             }
-            const defaultApply = cases[cases.length - 1];
+            var defaultApply = (_ref2 = arguments.length - 1, arguments.length <= _ref2 ? undefined : arguments[_ref2]);
             if (typeof defaultApply === 'function') {
                 return defaultApply(pattern);
             }
@@ -81,8 +80,8 @@ function match(pattern, passthrough = false) {
 }
 exports.match = match;
 exports.Test = {
-    isTuple,
-    is,
-    apply,
-    eq
+    isTuple: isTuple,
+    is: is,
+    apply: apply,
+    eq: eq
 };
